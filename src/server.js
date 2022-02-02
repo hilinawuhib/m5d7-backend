2fa8c0be067757624245c8ca7bb5f82ef075e872
hilina88 <hilinawuhib@gmail.com>
2022-02-02 14:13:11 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,27 @@ import path from "path";
 const publicPath = path.join(process.cwd(), "public");
 
 const server = express();
+const whiteListedOrigins = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
+
+console.log("Permitted origins:")
+console.table(whiteListedOrigins)
+
+server.use(
+  cors({
+    origin: function (origin, next) {
+      console.log("ORIGIN: ", origin)
+      // CORS is a global middleware --> for each and every request we are going to be able to read the origin value
+      if (!origin || whiteListedOrigins.indexOf(origin) !== -1) {
+        // indexOf returns -1 if element is NOT in the array
+        console.log("YAY!")
+        next(null, true)
+      } else {
+        // if the origin is NOT in the whitelist I should trigger an error
+        next(new Error("CORS ERROR!"))
+      }
+    },
+  })
+)
 
 server.use(express.static(publicPath));
 server.use(express.json());
