999ec01703dbe6e04827cdf8934d3ffad0fe1e3c
hilina88 <hilinawuhib@gmail.com>
2022-02-03 13:09:28 +0100

    Update fs-tools.js

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,19 +1,14 @@
-import fs from "fs-extra" // 3rd party module
+import fs from "fs-extra" 
 import { fileURLToPath } from "url"
 import { join, dirname } from "path"
-
 const { readJSON, writeJSON, writeFile , createdReadStream } = fs
-
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
 const usersPublicFolderPath = join(process.cwd(), "./public/img/users")
-
 const blogsJSONPath = join(dataFolderPath, "blogs.json")
 const authorsJSONPath = join(dataFolderPath, "authors.json")
-
 export const getBlogs = () => readJSON(blogsJSONPath)
 export const writeBlogs = content => writeJSON(blogsJSONPath, content)
 export const getAuthors = () => readJSON(authorsJSONPath)
 export const writeAuthors = content => writeJSON(authorsJSONPath, content)
-
 export const saveAuthorsAvatars = (filename, contentAsABuffer) => writeFile(join(usersPublicFolderPath, filename), contentAsABuffer)
 export const getBlogsReadableStream = () => createReadStream(blogsJSONPath)
